5ba3922d3ae3937de552c37dc33b79d03976718d
Joonyoung Sung <sungso376@gmail.com>
2024-03-27 19:36:21 +0900

    first commit

---
+++ caps 2.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	printf("숫자 하나를 입력하세요 : ");
+	scanf("%d", &a);
+
+	printf("입력한 숫자는 %d입니다.\n", a);
+
+	return 0;
+}
+
+//주석
+/*주석
+주석
+주석
+
+*/
