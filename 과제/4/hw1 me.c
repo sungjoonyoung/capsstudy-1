5a91d6379294470674f0061be02307097daadb56
Joonyoung Sung <sungso376@gmail.com>
2024-05-08 18:43:18 +0900

    looto re

--- 과제/4/hw1 me.c
+++ 과제/4/hw1 me.c
@@ -14,20 +14,62 @@
 //#include <stdlib.h>
 //#include <time.h>
 //#define _CRT_SECURE_NO_WARNINGS
-//srand((unsigned)time(NULL));
+//
 //int lotto[6] = { 0 };
 //
-//int right(int);
-//
+//int right(int,int);
+//void mix(int, int);
 //
 //int main(void) {
-//    int cash = 0;
+//    srand((unsigned)time(NULL));
+//    
 //    for (int i = 0; i < 6; i++) {
+//        int cash = 1;
 //        while (cash == 1) {
 //            int tmp;
-//            tmp = ran() % 45 + 1;
-//            cash = right(tmp);
+//            tmp = rand() % 45 + 1;
+//            lotto[i] = tmp;
+//            cash = right(tmp, i);
 //        }
 //    }
+//
+//    printf("%d ", lotto[0]);
+//    printf("%d ", lotto[1]);
+//    printf("%d ", lotto[2]);
+//    printf("%d ", lotto[3]);
+//    printf("%d ", lotto[4]);
+//    printf("%d \n", lotto[5]);
 //    
+//    for (int i = 0; i < 6; i++) {
+//        mix(4, 5);
+//    }
+//    
+//
+//    printf("%d ", lotto[0]);
+//    printf("%d ", lotto[1]);
+//    printf("%d ", lotto[2]);
+//    printf("%d ", lotto[3]);
+//    printf("%d ", lotto[4]);
+//    printf("%d ", lotto[5]);
+//
+//}
+//
+//void mix(x, y) {
+//    if (x == -1) return 0;
+//    if (lotto[x] > lotto[y]) {
+//        int tmp = 0;
+//        tmp = lotto[y];
+//        lotto[y] = lotto[x];
+//        lotto[x] = tmp;
+//    }
+//    mix(x - 1, y - 1);
+//}
+//
+//
+//int right(x,y) {
+//    if (y == 0) return 0;
+//    for (int i = 0; i < y; i++) {
+//        if (x == lotto[i]) return 1;
+//    }
+//    return 0;
 //}
